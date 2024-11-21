6bef0ace294aa1c1a95121f1147761ac6fbfb945
MahmoudFGhazal <mahmoud.f.ghazal@gmail.com>
2024-11-21 14:47:36 -0300

    melhoria no mover e no boxstock

--- src/main/java/com/fatec/farmmod/FarmMod.java
+++ src/main/java/com/fatec/farmmod/FarmMod.java
@@ -3,13 +3,10 @@ package com.fatec.farmmod;
 import com.fatec.farmmod.item.ModCreativeModeTabs;
 import com.fatec.farmmod.item.ModItems;
 import com.fatec.farmmod.event.ModEvents;
-import com.mojang.logging.LogUtils;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.CreativeModeTab; // Importação necessária
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.GameType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -21,14 +18,11 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
 
 
 @Mod(FarmMod.MOD_ID)
 public class FarmMod {
     public static final String MOD_ID = "farmmod";
-
-    private static final Logger LOGGER = LogUtils.getLogger();
 
 
     public FarmMod(FMLJavaModLoadingContext context) {
@@ -83,15 +77,4 @@ public class FarmMod {
             player.getInventory().add(new ItemStack(ModItems.BOXSTOCK.get()));
         }
     }
-
-    /*
-    //não deixa o jogador mudar de modo de jogo(fica no aventura)
-    @SubscribeEvent
-    public static void onPLayerChangeGameMode(PlayerEvent.PlayerChangeGameModeEvent event) {
-        if(event.getNewGameMode() != GameType.ADVENTURE){
-            event.setCanceled(true);
-            event.getEntity().sendSystemMessage(Component.literal("você não pode mudar o modo de jogo!"));
-        }
-    }
-    */
 }
